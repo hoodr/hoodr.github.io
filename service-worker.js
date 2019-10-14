16a3ff439f07e5928d71aa881427fc49b8cdeff9
Drew Hoo <drewhoo.8@gmail.com>
2019-10-14 08:39:19 -0400

    Build 2 10-14-2019

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.edfc9ca5bd4f045d4f5ee8285759173a.js"
+  "/precache-manifest.e7596c099dd71dd6df991d21d0ca7736.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -34,6 +34,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
+
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
