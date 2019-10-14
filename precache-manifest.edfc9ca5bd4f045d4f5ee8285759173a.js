6dd61e45feb6e4c8e82176b757f7986b41ee62dd
Drew Hoo <drewhoo.8@gmail.com>
2019-10-14 01:51:09 -0400

    Commit build 1 on 10-14-2019

---
+++ precache-manifest.edfc9ca5bd4f045d4f5ee8285759173a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb0cfd654a6cc6aac20bc1f6eab305c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74b6d071a301cf19ada0",
+    "url": "/static/css/main.2f27382b.chunk.css"
+  },
+  {
+    "revision": "b0e992101719fa7570c8",
+    "url": "/static/js/2.3c132ae5.chunk.js"
+  },
+  {
+    "revision": "74b6d071a301cf19ada0",
+    "url": "/static/js/main.34af021e.chunk.js"
+  },
+  {
+    "revision": "c6da2d683f9983980acb",
+    "url": "/static/js/runtime-main.5ea6b42a.js"
+  },
+  {
+    "revision": "5e40082ddac5bd03a385e5b072eb6c0f",
+    "url": "/static/media/arrow_2.5e40082d.svg"
+  },
+  {
+    "revision": "5e4b5cad89fb890aed181f59e08ff8fb",
+    "url": "/static/media/arrow_main.5e4b5cad.svg"
+  }
+]);
--- temp_storage/README.md
+++
@@ -1,2 +0,0 @@
-# hoodr.github.io -> [drewhoo.com](http://drewhoo.com/)
-This is my personal website and photo portfolio. Check out my site for more information.
